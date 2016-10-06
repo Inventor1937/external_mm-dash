9b6156a143deb9eb9ce36bf861160ce0d020d8da
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-06 18:50:11 +0200

    Please don't build QCMediaPlayer at all if not enabled
    Change-Id: Ic71bf6628f1937ad7e894629f47dcc49de570661

--- Android.mk
+++ Android.mk
@@ -1,7 +1,9 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
 ifneq ($(TARGET_DISABLE_DASH),true)
+ifeq ($(TARGET_ENABLE_QC_MEDIAPLAYER),true)
 include $(QCOM_MEDIA_ROOT)/jni/Android.mk
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/native/Android.mk
+endif
 include $(QCOM_MEDIA_ROOT)/dashplayer/Android.mk
 endif
