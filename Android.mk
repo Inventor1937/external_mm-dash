870ba78b3b6c1166df80c4c6d32760f221669b01
Simao Gomes Viana <xdevs23@outlook.com>
2016-10-06 19:14:10 +0200

    Add some checks for determiting whether to build or not
    Change-Id: I05b0b146b43661cdf6df32f1a2a28a6d97b8bb06

--- Android.mk
+++ Android.mk
@@ -1,5 +1,22 @@
 QCOM_MEDIA_ROOT := $(call my-dir)
-ifneq ($(TARGET_DISABLE_DASH),true)
+
+LOCAL_BUILD_DASH ?= false
+
+ifeq ($(TARGET_ENABLE_DASH),true)
+  LOCAL_BUILD_DASH := true
+endif
+
+ifeq ($(TARGET_DISABLE_DASH),true)
+  LOCAL_BUILD_DASH := false
+endif
+
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
+  LOCAL_BUILD_DASH := true
+else
+  LOCAL_BUILD_DASH := false
+endif
+
+ifeq ($(LOCAL_BUILD_DASH),true)
 ifeq ($(TARGET_ENABLE_QC_MEDIAPLAYER),true)
 include $(QCOM_MEDIA_ROOT)/jni/Android.mk
 include $(QCOM_MEDIA_ROOT)/QCMediaPlayer/Android.mk
